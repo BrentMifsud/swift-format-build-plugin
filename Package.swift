65fceace675a2a0aff1a24a6bad490731a8df125
Brent Mifsud <18543934+BrentMifsud@users.noreply.github.com>
2024-02-11 00:45:23 -0500

    Add Lint and Format build plugins

--- Package.swift
+++ Package.swift
@@ -1,22 +1,42 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-format-build-plugin",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
-        // Products can be used to vend plugins, making them visible to other packages.
         .plugin(
-            name: "swift-format-build-plugin",
-            targets: ["swift-format-build-plugin"]),
+            name: "FormatBuildPlugin",
+            targets: ["Format Build Plugin"]
+        ),
+        .plugin(
+            name: "LintBuildPlugin",
+            targets: ["Lint Build Plugin"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-format.git", from: Version(509, 0, 0)),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .plugin(
-            name: "swift-format-build-plugin",
-            capability: .buildTool()
+          name: "Format Build Plugin",
+          capability: .buildTool(),
+          dependencies: [
+              .product(name: "swift-format", package: "swift-format")
+          ],
+          path: "Plugins/FormatBuildPlugin"
         ),
+        .plugin(
+            name: "Lint Build Plugin",
+            capability: .buildTool(),
+            dependencies: [
+                .product(name: "swift-format", package: "swift-format")
+            ],
+            path: "Plugins/LintBuildPlugin"
+        )
     ]
 )
